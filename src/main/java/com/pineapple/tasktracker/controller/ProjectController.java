0fc20127e6aa359c6a29d6c0eed306c08ffea5e0
OneilN <aizatjedisg@gmail.com>
2020-12-26 18:33:02 +0300

    Added Welcome and Registration pages

--- src/main/java/com/pineapple/tasktracker/controller/ProjectController.java
+++ src/main/java/com/pineapple/tasktracker/controller/ProjectController.java
@@ -1,7 +1,7 @@
 package com.pineapple.tasktracker.controller;
 
-import java.util.List;
-
+import com.pineapple.tasktracker.model.Project;
+import com.pineapple.tasktracker.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,17 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.pineapple.tasktracker.model.Project;
-import com.pineapple.tasktracker.repository.ProjectRepository;
+import java.util.List;
 
 @Controller
 public class ProjectController {
 
 	@Autowired
 	private ProjectRepository projectRepository;
-
+	
+	@GetMapping("/welcome")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		name = "AizaT";
+		model.addAttribute("name", name);
+		return "index";
+	}
 	
 	@RequestMapping(value="/projects", method=RequestMethod.GET)
 	public String projects(Model model)
