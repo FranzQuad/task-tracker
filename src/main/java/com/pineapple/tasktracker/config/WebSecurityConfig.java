66a9d7cc01fb0acabaa40632806782d33619cfe1
OneilN <aizatjedisg@gmail.com>
2020-12-26 19:12:57 +0300

    Some changes

--- src/main/java/com/pineapple/tasktracker/config/WebSecurityConfig.java
+++ src/main/java/com/pineapple/tasktracker/config/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/", "/home").permitAll()
+				.antMatchers("/", "/registration", "/css/", "/js/", "/webjars/**").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
