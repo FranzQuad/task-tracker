b0bbc8c553c6e39b556f812a9e31d7e720f57bc8
OneilN <aizatjedisg@gmail.com>
2020-12-27 22:32:07 +0300

    Added Log out buttton

--- src/main/java/com/pineapple/tasktracker/controller/AddProjectController.java
+++ src/main/java/com/pineapple/tasktracker/controller/AddProjectController.java
@@ -1,15 +1,21 @@
 package com.pineapple.tasktracker.controller;
 
+import com.pineapple.tasktracker.model.Issue;
+import com.pineapple.tasktracker.model.Project;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 public class AddProjectController {
 
-    @GetMapping("/addproject")
-    public String addProject(Model model) {
-
-        return "addproject";
+    @RequestMapping(value="/addproject", method= RequestMethod.GET)
+    public String projects(Model model)
+    {
+        return "addproject";//here your name of your view (html)
     }
 }
