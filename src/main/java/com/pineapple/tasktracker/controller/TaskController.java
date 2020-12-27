043f1d7ee8251ad3b09b3561c69d95a4c4f54357
OneilN <aizatjedisg@gmail.com>
2020-12-28 02:44:07 +0300

    Added Task page and fixed bug with name on navigation bar

---
+++ src/main/java/com/pineapple/tasktracker/controller/TaskController.java
@@ -0,0 +1,27 @@
+package com.pineapple.tasktracker.controller;
+
+import com.pineapple.tasktracker.model.Issue;
+import com.pineapple.tasktracker.repository.IssueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+public class TaskController {
+
+    @Autowired
+    private IssueRepository issueRepository;
+
+    @RequestMapping(value="/task", method= RequestMethod.GET)
+    public String projects(Model model)
+    {
+        List<Issue> issues = (List<Issue>) issueRepository.findAll();
+        model.addAttribute("issues", issues);
+
+        return "task";//here your name of your view (html)
+    }
+}
